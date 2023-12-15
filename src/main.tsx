82633f4e477db522782e82a3314e43b9f805dfde
Илья <iliakekin52@gmail.com>
2023-12-16 02:38:07 +0300

    фг6шгу75шк7

--- src/main.tsx
+++ src/main.tsx
@@ -14,6 +14,10 @@ const router = createBrowserRouter([
   {
     path: '/vybory',
     element: <VoteList/>
+  },
+  {
+    path: '/',
+    element: <a href="/vybory">выборы</a>
   }
 ])
 ReactDOM.createRoot(document.getElementById('root')!).render(
