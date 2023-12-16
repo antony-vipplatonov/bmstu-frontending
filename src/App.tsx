d06e89e191e3dd66752023f82a31046b0c17e720
Илья <iliakekin52@gmail.com>
2023-12-16 03:19:17 +0300

    htynjyim

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,11 @@ import React from 'react'
 
 import VoteList from './VoteList.tsx'
 import VotingPage from './Voting.tsx';
-import BasicExample from './components/navbar'
 import {BrowserRouter as HashRouter,Route,Routes} from 'react-router-dom'
 //import Header from "./Header.tsx";
 const App: React.FC = () => {
     return (
         <HashRouter>
-            <BasicExample></BasicExample>
             <Routes>
                 <Route path="/vybory/" element={<VoteList/>}/>
                 <Route path="/vybory/:id" element={<VotingPage/>}/>
