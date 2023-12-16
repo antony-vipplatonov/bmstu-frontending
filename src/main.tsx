4140214bf600e10a7a9869aa71d85d47b5377f2c
Илья <iliakekin52@gmail.com>
2023-12-17 01:23:36 +0300

    rjuj

--- src/main.tsx
+++ src/main.tsx
@@ -2,24 +2,25 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import VoteList from './VoteList.tsx'
 import VotingPage from './Voting.tsx'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { createBrowserRouter, RouterProvider, Outlet } from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import NavBar from './components/navbar'
 
 const router = createBrowserRouter(
   [
   {
-    path: '/bmstu-frontend/vybory/:id',
-    element: <VotingPage/>
-  },
+    path: '/bmstu-frontend/',
+    element: <Outlet/>,
+    children: [
   {
     path: '/bmstu-frontend/vybory',
     element: <VoteList/>
   },
   {
-    path: '/bmstu-frontend/',
-    element: <a href='/bmstu-frontend/vybory'><h1>TEST SUCCESS</h1></a>
-  }
+    path: '/bmstu-frontend/vybory/:id',
+    element: <VotingPage/>
+  }]
+}
 ])
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
